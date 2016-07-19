02474bfdec6dfec40a4cad6ac4593616763935b1
Yamesh <seupera@hotmail.com>
2016-07-18 22:59:09 -0300

    Agora baseiam-se no usuario
    Essas classes alteram, inserem e removem item da tabela despensapessoal_usuario, tabela diferente para cada usuário

--- EstoqueDom/src/br/edu/ufabc/estoque/acao/AlteraItem.java
+++ EstoqueDom/src/br/edu/ufabc/estoque/acao/AlteraItem.java
@@ -6,9 +6,12 @@ import java.sql.Connection;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import br.edu.ufabc.estoque.modelo.Item;
+import br.edu.ufabc.estoque.modelo.Usuario;
 import br.edu.ufabc.estoque.dao.ItemDAO;
+import br.edu.ufabc.estoque.dao.ItemDAOcomUsuario;
 
 
 
@@ -18,6 +21,7 @@ public void executa(HttpServletRequest req, HttpServletResponse resp)
 			throws Exception {
 	
 	// obtem parametros do request
+//	String usuario = req.getParameter("usuario");
 	String nome = req.getParameter("nome");
 	double qtde = Double.valueOf(req.getParameter("qtde"));
 	double qtdeC = Double.valueOf(req.getParameter("qtdeC"));
@@ -31,12 +35,21 @@ public void executa(HttpServletRequest req, HttpServletResponse resp)
 	// instancia objeto Item
 	Item item = new Item(longId,nome,qtde,qtdeC,emFalta);
 
-
-	// Cria alunoDAO com a conexao anterior
 	//1 :> Obtem a conexão do FiltroBD;
 	Connection conexao = (Connection) req.getAttribute("conexao");
-	ItemDAO dao = new ItemDAO(conexao);
-	dao.altera(item);
+	
+	//2 :> obtem nome do usuario da sessão
+			HttpSession sessao = req.getSession();
+			Object objetoUsuario = sessao.getAttribute("usuario");
+			Usuario usuario = (Usuario) objetoUsuario;
+			String nomeDeUsuario = usuario.getUsuario();
+	
+	//3 :> cria ItemDAOcomUsuario
+	ItemDAOcomUsuario dao = new ItemDAOcomUsuario(nomeDeUsuario, conexao);
+	
+	//4 :> Chama a função com usuario
+	dao.altera(nomeDeUsuario, item);
+
 
 	req.setAttribute("msg", "Item: " + item.getNome()
 	+ " alterado com sucesso!");
--- EstoqueDom/src/br/edu/ufabc/estoque/acao/InsereItem.java
+++ EstoqueDom/src/br/edu/ufabc/estoque/acao/InsereItem.java
@@ -6,12 +6,9 @@ import java.sql.Connection;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import br.edu.ufabc.estoque.dao.ItemDAO;
-import br.edu.ufabc.estoque.dao.ItemDAOcomUsuario;
 import br.edu.ufabc.estoque.modelo.Item;
-import br.edu.ufabc.estoque.modelo.Usuario;
 
 
 
@@ -21,26 +18,20 @@ public void executa(HttpServletRequest req, HttpServletResponse resp)
 			throws Exception {
 		
 		// obtem parametros do request
-
 		String nome = req.getParameter("nome");
 		double qtde = Double.valueOf(req.getParameter("qtde"));
 		double qtdeC = Double.valueOf(req.getParameter("qtdeC"));
 		boolean emFalta = Boolean.valueOf(req.getParameter("emFalta"));
 		
-		// obtem nome do usuario da sessão
-		HttpSession sessao = req.getSession();
-		Object objetoUsuario = sessao.getAttribute("usuario");
-		Usuario usuario = (Usuario) objetoUsuario;
-		String nomeDeUsuario = usuario.getUsuario();
-		
+
 		// instancia objeto Item
 		Item item = new Item(nome,qtde,qtdeC,emFalta);
 
 		// Cria alunoDAO com a conexao anterior
-		//1 :> Obtem a conexão do FiltroBD;
+		//1 :> Obtem a conex�o do FiltroBD;
 		Connection conexao = (Connection) req.getAttribute("conexao");
-		ItemDAOcomUsuario dao = new ItemDAOcomUsuario(nomeDeUsuario, conexao);
-		dao.insere(nomeDeUsuario, item);
+		ItemDAO dao = new ItemDAO(conexao);
+		dao.insere(item);
 
 		req.setAttribute("msg", "Item: " + item.getNome()
 		+ " inserido com sucesso!");

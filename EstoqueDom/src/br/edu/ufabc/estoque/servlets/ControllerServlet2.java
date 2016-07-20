26ed27c197aee57469e65e7a2e2412de1023616b
Yamesh <seupera@hotmail.com>
2016-07-20 20:36:18 -0300

    Controller para usar com a nova index
    Ele não redireciona a pagina para um "sucesso.html", mas recarrega a mesma pagina.

---
+++ EstoqueDom/src/br/edu/ufabc/estoque/servlets/ControllerServlet2.java
@@ -0,0 +1,52 @@
+package br.edu.ufabc.estoque.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.annotation.WebServlet;
+
+import br.edu.ufabc.estoque.acao.Acao;
+
+
+@WebServlet("/controller2")
+public class ControllerServlet2 extends HttpServlet{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		//(opcao) recebe a opção da operação(insere, altera, busca, remove)
+			String opcao = req.getParameter("opcao");
+		
+		//Define que classe será chamada, baseado na opcao	
+			String nomeDaClasse = "br.edu.ufabc.estoque.acao." + opcao;
+		
+		//String com parte do texto a ser impresso
+			String textoDeSaida;
+			try {
+				Class<?> classe = Class.forName(nomeDaClasse);
+				Acao acao = (Acao) classe.newInstance();
+
+				acao.executa(req, resp);
+				
+				RequestDispatcher rd = req.getRequestDispatcher("/admin/index.jsp");
+				rd.forward(req, resp);
+			
+			
+			} catch (Exception e) {
+				throw new ServletException("Erro: ",e);
+			}
+		}
+	}
+	
+		
+		
